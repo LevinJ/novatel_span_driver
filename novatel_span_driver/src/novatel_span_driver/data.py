118aa9033ad36fb00f57944ce00a4afc6d69f640
Levin Jian <jianzhirong@gmail.com>
2019-06-18 10:07:13 +0800

    add traces in span driver

--- novatel_span_driver/src/novatel_span_driver/data.py
+++ novatel_span_driver/src/novatel_span_driver/data.py
@@ -72,7 +72,7 @@ class DataPort(Port):
                 if header.id not in bad_pkts:
                     rospy.logwarn("Error parsing %s.%d" % header.id)
                     bad_pkts.add(pkt)
-
+            print("processed message {}".format(header.id))        
             if header.id not in pkt_counters:
                 pkt_counters[header.id] = 0
             else:
